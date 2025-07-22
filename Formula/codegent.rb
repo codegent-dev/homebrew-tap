079d14b9a3102ee7a143504677c0c875c9448134
Matt Apperson <me@mattapperson.com>
2025-07-21 22:52:00 -0400

    Temporarily disable formula until releases are available

--- Formula/codegent.rb
+++ Formula/codegent.rb
@@ -1,27 +1,16 @@
 class Codegent < Formula
   desc "AI-powered workflow orchestration CLI"
   homepage "https://codegent.dev"
-  version "2.0.0"
-
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/codegent-dev/cli-releases/releases/download/v2.0.0/codegent-darwin-arm64"
-      sha256 "08c197eee49f53bab9df7b5f8a426a2d40c9d05eb8859924a0ea2d780662d8fe"
-    else
-      url "https://github.com/codegent-dev/cli-releases/releases/download/v2.0.0/codegent-darwin-x64"
-      sha256 "37b0563b022574e25c3c277f04f2af3ac05540e5fc76b6f00df3f407c8428e1b"
-    end
-  end
-
+  
+  # Temporarily disabled - no releases available yet
+  # Run the release script to create a new release
+  
   def install
-    if Hardware::CPU.arm?
-      bin.install "codegent-darwin-arm64" => "codegent"
-    else
-      bin.install "codegent-darwin-x64" => "codegent"
-    end
+    odie "No releases available yet. Please run the release script to create a release."
   end
-
+  
   test do
-    system "#{bin}/codegent", "--version"
+    # Test will be added when releases are available
+    true
   end
 end
